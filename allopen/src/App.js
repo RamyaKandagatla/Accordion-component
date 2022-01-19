812590400bb3f1158532a9b1cb7556ace70c04f9
RAMYA <kandagatlaramya5@gmail.com>
2022-01-19 10:00:25 +0530

    in all possible ways

--- allopen/src/App.js
+++ allopen/src/App.js
@@ -1,12 +1,12 @@
+import React,{useState} from 'react';
 import './App.css';
+import {data} from './components/data';
 
 function App() {
+const [opened,setOpened]=useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        
-          
-      </header>
+      
     </div>
   );
 }
